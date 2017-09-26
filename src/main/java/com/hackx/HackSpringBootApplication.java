d6dc4ba0838fa9eb7bb27d54e6d4d769582d9283
曹磊(hackx) <caolei@mobike.com>
2017-09-26 08:02:07 +0800

    fix bug

--- src/main/java/com/hackx/HackSpringBootApplication.java
+++ src/main/java/com/hackx/HackSpringBootApplication.java
@@ -1,12 +1,14 @@
 package com.hackx;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAdminServer
 @SpringBootApplication
 public class HackSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HackSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HackSpringBootApplication.class, args);
+    }
 }
